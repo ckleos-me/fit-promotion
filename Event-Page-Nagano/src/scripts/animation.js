587cb6a10bba1380b8c46669de597a4d4b805476
ckleo Siu <67173226+ckleo-design@users.noreply.github.com>
2025-06-08 01:30:13 +0900

    client-FB-fixed

--- Event-Page-Nagano/src/scripts/animation.js
+++ Event-Page-Nagano/src/scripts/animation.js
@@ -9,3 +9,35 @@ export function smoothScroll() {
         $('html, body').animate({ scrollTop: position }, 500);
     });
 }
+
+// scroll to top
+$('.totop').click(function () {
+    $('body,html').animate(
+        {
+            scrollTop: 0
+        },
+        500
+    );
+    return false;
+});
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200) {
+        //上から200pxスクロールしたら
+        $('.totop').addClass('show'); //.totopについているUpMoveというクラス名を付与
+    } else {
+        $('.totop').removeClass('show'); //UpMoveというクラス名を除き
+    }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+    PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+    PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
